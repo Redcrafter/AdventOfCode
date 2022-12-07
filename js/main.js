c0d19fe61cbe19f7471801a1368a1bc9d9a16b45
Redcrafter <redcrafter@hotmail.de>
2022-12-07 17:31:15 +0100

    improved benchmark output

--- js/main.js
+++ js/main.js
@@ -3,6 +3,10 @@ function now() {
     return process.hrtime.bigint();
 }
 
+function format(n) {
+    return (Number(n) / 1000).toFixed(1).padStart(7, " ") + "μs";
+}
+
 /**
  * @param {number} day 
  * @param {number} part 
@@ -17,37 +21,61 @@ function runTest(func) {
 
     let i = 0;
     let time = 0n;
-    while (time < 20_000_000_000) {
+    let min = 9999999999999n;
+    let max = 0n;
+
+    while (time < 10_000_000_000) {
         let start = now();
         func();
         let end = now();
 
-        time += end - start;
+        let dt = end - start;
+
+        if (dt < min) min = dt;
+        if (dt > max) max = dt;
+
+        time += dt;
         i++;
     }
-    time /= BigInt(i * 1000);
 
-    return Number(time).toFixed(4).padStart(10, " ") + "μs";
+    return [min, max, time / BigInt(i)];
 }
 
 async function doDay(year, day) {
     let mod = await import(`./${year}/day-${day}.js`);
 
     let p1 = runTest(mod.part1);
-    console.log(`${year}-${day.toString().padStart(2)}-${1} took ${p1}`);
+    console.log(`${year} ${day.toString().padStart(2)}-${1} ${p1.map(format).join(" ")}`);
 
     let p2 = runTest(mod.part2);
-    console.log(`${year}-${day.toString().padStart(2)}-${2} took ${p2}`);
+    console.log(`${year} ${day.toString().padStart(2)}-${2} ${p2.map(format).join(" ")}`);
 
     return [p1, p2];
 }
 
+console.log("                min       max   average");
+
 let table = [];
-for (const year of [2020, 2021, 2022]) {
+for (const year of [2022]) {
     for (let i = 1; i <= 25; i++) {
         try {
             let res = await doDay(year, i);
-            table.push({ year, day: i, part1: res[0], part2: res[1] });
+            table.push({
+                year,
+                day: i,
+                part: 1,
+                min: format(res[0][0]),
+                max: format(res[0][1]),
+                avg: format(res[0][2])
+            });
+            table.push({
+                year,
+                day: i,
+                part: 2,
+                min: format(res[1][0]),
+                max: format(res[1][1]),
+                avg: format(res[1][2])
+            });
         } catch (e) { }
     }
 }
