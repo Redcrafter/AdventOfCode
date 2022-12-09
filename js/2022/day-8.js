25ec6dcef0d8399e096f0e1c1b9401ab9fafbe1e
Redcrafter <redcrafter@hotmail.de>
2022-12-09 12:58:36 +0100

    optimized day 8

--- js/2022/day-8.js
+++ js/2022/day-8.js
@@ -3,8 +3,9 @@ const input = getInput().flatMap(x => [...x].map(x => +x));
 
 const size = 99;
 
-function calcSet() {
-    let vis = new Set();
+export function part1() {
+    let visited = new Uint8Array(99 * 99);
+    let res = 0;
 
     function ray(x, y, dx, dy) {
         let height = -1;
@@ -13,7 +14,9 @@ function calcSet() {
             let v = input[x + y * size];
             if (v > height) {
                 height = v;
-                vis.add(x + y * size);
+
+                if (visited[x + y * size] == 0) res++;
+                visited[x + y * size] = 1;
             }
 
             x += dx;
@@ -29,14 +32,13 @@ function calcSet() {
         ray(i, 98, 0, -1);
     }
 
-    return vis;
-}
-
-export function part1() {
-    return calcSet().size;
+    return res;
 }
 
 export function part2() {
+    let best = 0;
+    let visited = new Uint8Array(99 * 99);
+
     function score(x, y, dx, dy) {
         let height = input[x + y * size];
 
@@ -58,14 +60,32 @@ export function part2() {
         return score;
     }
 
-    let best = 0;
-    let vis = calcSet();
-    for (const item of vis) {
-        let x = item % size;
-        let y = (item / size) | 0;
+    function ray(x, y, dx, dy) {
+        let height = -1;
 
-        let s = score(x, y, 1, 0) * score(x, y, -1, 0) * score(x, y, 0, 1) * score(x, y, 0, -1);
-        if (s > best) best = s;
+        while (height < 9 && x < size && y < size & x >= 0 && y >= 0) {
+            let v = input[x + y * size];
+            if (v > height) {
+                height = v;
+
+                if (visited[x + y * size] == 0) {
+                    let s = score(x, y, 1, 0) * score(x, y, -1, 0) * score(x, y, 0, 1) * score(x, y, 0, -1);
+                    if (s > best) best = s;
+                };
+                visited[x + y * size] = 1;
+            }
+
+            x += dx;
+            y += dy;
+        }
+    }
+
+    for (let i = 0; i < size; i++) {
+        ray(0, i, 1, 0);
+        ray(98, i, -1, 0);
+
+        ray(i, 0, 0, 1);
+        ray(i, 98, 0, -1);
     }
 
     return best;
