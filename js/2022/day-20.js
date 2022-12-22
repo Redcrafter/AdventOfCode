75d494aaebb691f84da2d9493f3a4c84f0af3b8f
Redcrafter <redcrafter@hotmail.de>
2022-12-22 22:34:52 +0100

    made day 20 run 10x faster

--- js/2022/day-20.js
+++ js/2022/day-20.js
@@ -3,6 +3,8 @@ import { extractNumbers, getInput } from "../util.js";
 const input = extractNumbers(getInput()).map(x => x[0]);
 const h = input.length / 2;
 
+const jumpSize = 20;
+
 function buildList(mul) {
     let ll = [];
 
@@ -13,7 +15,9 @@ function buildList(mul) {
         let el = {
             val: item * mul,
             prev: last,
-            next: null
+            next: null,
+            prevLong: null,
+            nextLong: null,
         }
         if (last) last.next = el;
         last = el;
@@ -23,6 +27,16 @@ function buildList(mul) {
     }
     ll[0].prev = last;
     last.next = ll[0];
+
+    let a = ll[0];
+    let b = ll[jumpSize];
+    for (let i = 0; i < ll.length; i++) {
+        a.nextLong = b;
+        b.prevLong = a;
+
+        a = a.next;
+        b = b.next;
+    }
 
     return [ll, zero];
 }
@@ -38,15 +52,38 @@ function doLoop(ll) {
         el.prev.next = el.next;
         el.next.prev = el.prev;
 
+        {
+            let a = el.prevLong;
+            let b = el.next;
+
+            for (let i = 0; i < jumpSize; i++) {
+                a.nextLong = b;
+                b.prevLong = a;
+                
+                a = a.next;
+                b = b.next;
+            }
+        }
+
         let a = el;
         if (move < 0) {
-            for (let i = move; i < 0; i++) {
+            while (move <= -jumpSize) {
+                a = a.prevLong;
+                move += jumpSize;
+            }
+            while (move < 0) {
                 a = a.prev;
+                move++
             }
             a = a.prev;
         } else if (move > 0) {
-            for (let i = 0; i < move; i++) {
+            while (move >= jumpSize) {
+                a = a.nextLong;
+                move -= jumpSize;
+            }
+            while (move > 0) {
                 a = a.next;
+                move--;
             }
         }
         let b = a.next;
@@ -56,21 +93,34 @@ function doLoop(ll) {
 
         el.next = b;
         b.prev = el;
+
+        {
+            let a1 = b.prevLong;
+            let b1 = el;
+
+            for (let i = 0; i < jumpSize + 1; i++) {
+                a1.nextLong = b1;
+                b1.prevLong = a1;
+                
+                a1 = a1.next;
+                b1 = b1.next;
+            }
+        }
     }
 }
 
 function getAns(zero) {
     let a = zero;
-    for (let i = 0; i < 1000; i++) {
-        a = a.next;
+    for (let i = 0; i < 50; i++) {
+        a = a.nextLong;
     }
     let b = a;
-    for (let i = 0; i < 1000; i++) {
-        b = b.next;
+    for (let i = 0; i < 50; i++) {
+        b = b.nextLong;
     }
     let c = b;
-    for (let i = 0; i < 1000; i++) {
-        c = c.next;
+    for (let i = 0; i < 50; i++) {
+        c = c.nextLong;
     }
     return a.val + b.val + c.val;
 }
