e943f06758180dadca9fa50ba69aa5facd63472a
Redcrafter <redcrafter@hotmail.de>
2024-12-08 00:00:26 +0100

    moved day includes to separate file

--- c++/main.cpp
+++ c++/main.cpp
@@ -6,60 +6,7 @@
 #include <tuple>
 #include <vector>
 
-#ifdef _MSC_VER
-#include <windows.h>
-// y u do this windows
-#undef min
-#undef max
-#endif
-
-#include "./2021/day-1.hpp"
-#include "./2021/day-7.hpp"
-#include "./2021/day-11-bit.hpp"
-#include "./2021/day-11.hpp"
-#include "./2021/day-12.hpp"
-#include "./2021/day-13.hpp"
-#include "./2021/day-20.hpp"
-#include "./2021/day-21.hpp"
-
-#include "./2022/day-6.hpp"
-#include "./2022/day-7.hpp"
-#include "./2022/day-8.hpp"
-#include "./2022/day-9.hpp"
-#include "./2022/day-12.hpp"
-
-#include "./2023/day-1.hpp"
-#include "./2023/day-2.hpp"
-#include "./2023/day-3.hpp"
-#include "./2023/day-4.hpp"
-#include "./2023/day-5.hpp"
-#include "./2023/day-6.hpp"
-#include "./2023/day-7.hpp"
-#include "./2023/day-8.hpp"
-#include "./2023/day-9.hpp"
-#include "./2023/day-10.hpp"
-#include "./2023/day-11.hpp"
-#include "./2023/day-12.hpp"
-#include "./2023/day-13.hpp"
-#include "./2023/day-14.hpp"
-#include "./2023/day-15.hpp"
-#include "./2023/day-16.hpp"
-#include "./2023/day-17.hpp"
-#include "./2023/day-18.hpp"
-#include "./2023/day-19.hpp"
-#include "./2023/day-20.hpp"
-#include "./2023/day-21.hpp"
-#include "./2023/day-22.hpp"
-#include "./2023/day-23.hpp"
-#include "./2023/day-24.hpp"
-#include "./2023/day-25.hpp"
-
-#include "./2024/day-1.hpp"
-#include "./2024/day-2.hpp"
-#include "./2024/day-4.hpp"
-#include "./2024/day-5.hpp"
-#include "./2024/day-6.hpp"
-#include "./2024/day-7.hpp"
+#include "days.hpp"
 
 template <class T>
 inline void DoNotOptimize(const T& value) {
