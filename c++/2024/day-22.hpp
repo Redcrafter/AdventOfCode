095174b9ccd0bce3e07c7eaaddd9c139e035e66d
Redcrafter <redcrafter@hotmail.de>
2024-12-22 13:19:14 +0100

    vectorized 2024-22

--- c++/2024/day-22.hpp
+++ c++/2024/day-22.hpp
@@ -21,6 +21,38 @@ uint64_t part1() {
     return result;
 }
 
+uint64_t part1_simd() {
+    const auto mask = _mm256_set1_epi32(0xFFFFFF);
+    uint64_t result = 0;
+    alignas(32) int temp[8];
+
+    int i = 0;
+    for(; i + 8 <= input.size(); i += 8) {
+        auto nums = _mm256_loadu_si256((__m256i*)&input[i]);
+        for(int j = 0; j < 2000; ++j) {
+            nums = _mm256_and_si256(_mm256_xor_si256(nums, _mm256_slli_epi32(nums, 6)), mask);
+            nums = _mm256_xor_si256(nums, _mm256_srli_epi32(nums, 5));
+            nums = _mm256_xor_si256(nums, _mm256_slli_epi32(nums, 11));
+        }
+        nums = _mm256_and_si256(nums, mask);
+
+        _mm256_store_si256((__m256i*)temp, nums);
+        for(int j = 0; j < 8; ++j)
+            result += temp[j];
+    }
+
+    for(; i < input.size(); i++) {
+        auto num = input[i];
+        for(int j = 0; j < 2000; j++) {
+            num = (num ^ (num << 6)) & 0xFFFFFF;
+            num = num ^ (num >> 5);
+            num = num ^ (num << 11);
+        }
+        result += num & 0xFFFFFF;
+    }
+    return result;
+}
+
 uint64_t part2() {
     std::array<uint16_t, 1 << 20> possible{};
     std::array<bool, 1 << 20> sub{};
@@ -57,6 +89,7 @@ uint64_t part2() {
 
 static auto p = aoc::test([]() { return extractNumbers<int>(aoc::getInput(2024, 22)).size(); }, 2024, 22, 0, "parse");
 static auto p1 = aoc::test(part1, 2024, 22, 1, "part 1");
+static auto p1s = aoc::test(part1_simd, 2024, 22, 1, "part 1 simd");
 static auto p2 = aoc::test(part2, 2024, 22, 2, "part 2");
 
 } // namespace y2024::Day22
