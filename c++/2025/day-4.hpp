36aaf67fd271dbb4277584047966c2f1e67c3ac2
Redcrafter <redcrafter@hotmail.de>
2025-12-08 19:55:35 +0100

    slight improvements

--- c++/2025/day-4.hpp
+++ c++/2025/day-4.hpp
@@ -4,6 +4,7 @@
 
 #include "../aoc.hpp"
 #include "../fixedVector.hpp"
+#include "../grid.hpp"
 #include "../util.hpp"
 
 namespace y2025::Day4 {
@@ -13,82 +14,98 @@ const auto input = aoc::getInput(2025, 4);
 const auto height = 139;
 const auto width = 139;
 
+auto make_grid() {
+    Grid<width + 2, height + 2, char> grid; // 1 space padding
+
+    for(size_t y = 0; y < width + 2; y++) {
+        grid(y, 0) = 0;
+        grid(y, height + 1) = 0;
+        grid(0, y) = 0;
+        grid(width + 1, y) = 0;
+    }
+    for(size_t y = 0; y < 139; y++) {
+        for(size_t x = 0; x < 139; x++) {
+            grid(x + 1, y + 1) = input[x + y * (width + 1)] == '@';
+        }
+    }
+    return grid;
+}
+
 uint64_t part1() {
+    auto grid = make_grid();
+
     uint64_t res = 0;
+    for(int y = 1; y < height + 1; y++) {
+        for(int x = 1; x < width + 1; x++) {
+            auto c = grid(x, y);
+            if(c == 0) continue;
 
-    for (int y = 0; y < height; y++) {
-        for (int x = 0; x < width; x++) {
-            auto c = input[x + y * (width + 1)];
-            if (c != '@') continue;
+            auto tl = grid(x - 1, y - 1);
+            auto t  = grid(x + 0, y - 1);
+            auto tr = grid(x + 1, y - 1);
 
-            auto tl = y > 0 &&          x > 0         && input[x - 1 + (y - 1) * (width + 1)] == '@';
-            auto t  = y > 0 &&                           input[x + 0 + (y - 1) * (width + 1)] == '@';
-            auto tr = y > 0 &&          x + 1 < width && input[x + 1 + (y - 1) * (width + 1)] == '@';
+            auto l = grid(x - 1, y + 0);
+            auto r = grid(x + 1, y + 0);
 
-            auto l  =                   x > 0         && input[x - 1 + (y + 0) * (width + 1)] == '@';
-            auto r  =                   x + 1 < width && input[x + 1 + (y + 0) * (width + 1)] == '@';
+            auto bl = grid(x - 1, y + 1);
+            auto b  = grid(x + 0, y + 1);
+            auto br = grid(x + 1, y + 1);
 
-            auto bl = y + 1 < height && x > 0         && input[x - 1 + (y + 1) * (width + 1)] == '@';
-            auto b  = y + 1 < height &&                  input[x + 0 + (y + 1) * (width + 1)] == '@';
-            auto br = y + 1 < height && x + 1 < width && input[x + 1 + (y + 1) * (width + 1)] == '@';
-
-            if (tl + t + tr + l + r + bl + b + br < 4)
+            if(tl + t + tr + l + r + bl + b + br < 4)
                 res++;
         }
     }
-
     return res;
 }
 
 uint64_t part2() {
     uint64_t res = 0;
 
-    // make copy
-    auto inp = std::string(input);
+    auto grid = make_grid();
 
     std::vector<std::pair<int, int>> changed;
     std::vector<std::pair<int, int>> next;
 
     auto test = [&](int x, int y) {
-        auto c = inp[x + y * (width + 1)];
-        if (c != '@') return;
+        auto c = grid(x, y);
+        if(c == 0) return;
 
-        auto tl = y > 0 &&          x > 0         && inp[x - 1 + (y - 1) * (width + 1)] == '@';
-        auto t  = y > 0                           && inp[x + 0 + (y - 1) * (width + 1)] == '@';
-        auto tr = y > 0 &&          x + 1 < width && inp[x + 1 + (y - 1) * (width + 1)] == '@';
+        auto tl = grid(x - 1, y - 1);
+        auto t  = grid(x + 0, y - 1);
+        auto tr = grid(x + 1, y - 1);
 
-        auto l  =                   x > 0         && inp[x - 1 + (y + 0) * (width + 1)] == '@';
-        auto r  =                   x + 1 < width && inp[x + 1 + (y + 0) * (width + 1)] == '@';
+        auto l = grid(x - 1, y + 0);
+        auto r = grid(x + 1, y + 0);
 
-        auto bl = y + 1 < height && x > 0         && inp[x - 1 + (y + 1) * (width + 1)] == '@';
-        auto b  = y + 1 < height                  && inp[x + 0 + (y + 1) * (width + 1)] == '@';
-        auto br = y + 1 < height && x + 1 < width && inp[x + 1 + (y + 1) * (width + 1)] == '@';
+        auto bl = grid(x - 1, y + 1);
+        auto b  = grid(x + 0, y + 1);
+        auto br = grid(x + 1, y + 1);
 
-        if (tl + t + tr + l + r + bl + b + br < 4) {
+        if(tl + t + tr + l + r + bl + b + br < 4) {
             changed.emplace_back(x, y);
-            inp[x + y * (width + 1)] = '.';
+            grid(x, y) = 0;
             res++;
         }
     };
 
-    for (int y = 0; y < height; y++) {
-        for (int x = 0; x < width; x++) {
-            test(x, y);
+    for(int y = 0; y < height; y++) {
+        for(int x = 0; x < width; x++) {
+            test(x + 1, y + 1);
         }
     }
 
-    while (changed.size() > 0) {
+    while(changed.size() > 0) {
         std::swap(next, changed);
         changed.clear();
         for(auto& [x, y] : next) {
-            if(y > 0 &&          x > 0        ) test(x - 1, y - 1);
-            if(y > 0                          ) test(x + 0, y - 1);
-            if(y > 0 &&          x + 1 < width) test(x + 1, y - 1);
-            if(                  x > 0        ) test(x - 1, y);
-            if(                  x + 1 < width) test(x + 1, y);
-            if(y + 1 < height && x > 0        ) test(x - 1, y + 1);
-            if(y + 1 < height                 ) test(x + 0, y + 1);
-            if(y + 1 < height && x + 1 < width) test(x + 1, y + 1);
+            test(x - 1, y - 1);
+            test(x + 0, y - 1);
+            test(x + 1, y - 1);
+            test(x - 1, y);
+            test(x + 1, y);
+            test(x - 1, y + 1);
+            test(x + 0, y + 1);
+            test(x + 1, y + 1);
         }
     }
 
@@ -98,4 +115,4 @@ uint64_t part2() {
 static auto p1 = aoc::test(part1, 2025, 4, 1, "part1");
 static auto p2 = aoc::test(part2, 2025, 4, 2, "part2");
 
-} // namespace y2025::Day3
+} // namespace y2025::Day4
--- c++/2025/day-5.hpp
+++ c++/2025/day-5.hpp
@@ -1,6 +1,6 @@
 #pragma once
+#include <algorithm>
 #include <cmath>
-#include <print>
 
 #include "../aoc.hpp"
 #include "../fixedVector.hpp"
@@ -10,11 +10,7 @@ namespace y2025::Day5 {
 
 const auto input = aoc::getInput(2025, 5);
 
-uint64_t part1() {
-    uint64_t res = 0;
-
-    size_t pos = 0;
-
+auto parse_nums(size_t& pos) {
     std::vector<std::pair<int64_t, int64_t>> nums;
     while(true) {
         auto a = readUInt<int64_t>(input, pos);
@@ -25,56 +21,48 @@ uint64_t part1() {
     pos++;
 
     std::sort(nums.begin(), nums.end(), [](auto& a, auto& b) { return a.first < b.first; });
+    return nums;
+}
+
+uint64_t part1() {
+    size_t pos = 0;
+    auto nums = parse_nums(pos);
 
     std::vector<std::pair<int64_t, int64_t>> ranges;
 
+    // combine and remove overlapping ranges
     int64_t end = 0;
     for(size_t i = 0; i < nums.size(); i++) {
         const auto [a, b] = nums[i];
-        if(a - 1 <= end) {
-            if(b > end) {
-                // extend last range
+        if(a - 1 <= end) { // overlaps with previous
+            if(b > end) { // extend previous range
                 ranges.back().second = b;
             }
             // else completely contained = ignore
-        } else {
-            // at least 1 gap
+        } else { // at least 1 gap
             ranges.emplace_back(a, b);
         }
 
         end = std::max(end, b);
     }
 
+    uint64_t res = 0;
     while(pos < input.size()) {
         auto num = readUInt<int64_t>(input, pos);
-
-        for(auto [a, b] : ranges) {
-            if(num >= a && num <= b) {
-                res++;
-                break;
-            }
+        auto range = std::upper_bound(ranges.begin(), ranges.end(), num, [](const auto& a, const auto& b) { return a < b.second; });
+        if(num >= range->first && num <= range->second) {
+            res++;
         }
     }
-
     return res;
 }
 
 uint64_t part2() {
-    uint64_t res = 0;
     size_t pos = 0;
+    auto nums = parse_nums(pos);
 
-    std::vector<std::pair<int64_t, int64_t>> nums;
-    while(true) {
-        auto a = readUInt<int64_t>(input, pos);
-        auto b = readUInt<int64_t>(input, pos);
-        nums.emplace_back(a, b);
-        if(input[pos] == '\n') break;
-    }
-
-    std::sort(nums.begin(), nums.end(), [](auto& a, auto& b) { return a.first < b.first; });
-
+    uint64_t res = 0;
     int64_t end = 0;
-
     for(auto [a, b] : nums) {
         if(a <= end) {
             a = end + 1;
